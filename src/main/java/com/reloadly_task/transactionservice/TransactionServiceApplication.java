f06c1b528dc61890bf71d610ba3bff863f347d1f
Gadibia Daro <55360356+dizsart@users.noreply.github.com>
2022-03-19 14:23:36 +0100

    add system.properties

--- src/main/java/com/reloadly_task/transactionservice/TransactionServiceApplication.java
+++ src/main/java/com/reloadly_task/transactionservice/TransactionServiceApplication.java
@@ -3,10 +3,12 @@ package com.reloadly_task.transactionservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableEurekaClient
+@Profile("dev")
 public class TransactionServiceApplication {
 
     public static void main(String[] args) {
